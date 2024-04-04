eadf12b323223847fc763254f6badc967fbbfc83
Md.Ashraful Haque <ashrafulhaque@users.noreply.github.com>
2024-04-04 23:09:31 +0600

    Created new dir 'python' and programs into it

---
+++ python/006.write_a_function.py
@@ -0,0 +1,16 @@
+# Program: Write a function
+# Programmer: Md.Ashraful Haque
+# Date: 24.03.2024
+
+def is_leap(year):
+    leap = False
+    
+    if (year % 4) == 0 and (year % 100) != 0:
+        leap = True
+    elif (year % 100) == 0 and (year % 400 == 0):
+        leap = True
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))
---
+++ python/007.print_function.py
@@ -0,0 +1,13 @@
+# Program: Print Function
+# Programmer: Md.Ashraful Haque
+# Date: 24.03.2024
+
+n = int(input())
+temp = ""
+
+for i in range(1,n+1):
+    temp += str(i) 
+
+print(temp)    
+
+# Task: input: 5, output: 12345 (as string) 
