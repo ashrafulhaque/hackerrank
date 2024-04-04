761665885291b9ff8b91a263c94c59bce497d138
Md.Ashraful Haque <ashrafulhaque@users.noreply.github.com>
2024-04-04 22:10:05 +0600

    12th problem solved on HackerRank-Lists

---
+++ 12_lists.py
@@ -0,0 +1,31 @@
+# Program: Lists
+# Programmer: Md.Ashraful Haque
+# Date: 04.04.2024
+
+
+data_list = [] # list to keep all the numbers 
+
+N = int(input())
+
+for _ in range(N):
+    option = input().split()
+    operation = option[0]
+
+    if len(option) == 3:
+        if operation == "insert":
+            data_list.insert(int(option[1]),int(option[2]))
+    elif len(option) == 2:
+        if operation == "append":
+            data_list.append(int(option[1]))
+        elif operation == "remove":
+            data_list.remove(int(option[1]))
+    elif len(option) == 1:
+        if operation == "print":
+            print(data_list)
+        elif operation == "pop":
+            data_list.pop()
+        elif operation == "sort":
+            data_list.sort()
+        elif operation == "reverse":
+            data_list.reverse()
+        
