27a4735335169df8c81ca16f08338af5b40e46ab
Md.Ashraful Haque <ashrafulhaque@users.noreply.github.com>
2024-04-18 23:25:28 +0600

    Yet another string problem

---
+++ python/Strings/025.Capitalize.py
@@ -0,0 +1,26 @@
+# Problem: https://www.hackerrank.com/challenges/capitalize
+# Programmer: Md. Ashraful Haque
+# Date: 18.04.2024
+
+def solve(s):
+    name = list(s.split(" "))
+    final_name = ""
+    for i in name:
+            if len(final_name) == 0:
+                final_name +=  i.capitalize()
+            else:
+                 final_name += " " + i.capitalize()
+    return final_name
+         
+def main():
+    fptr = open("capitalize.txt", 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
+
+main()
