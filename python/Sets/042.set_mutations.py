0891ecf0898ddbcbf92a9ba1176132fff9e0b0e6
Md.Ashraful Haque <ashrafulhaque@users.noreply.github.com>
2024-04-23 21:40:47 +0600

    Sets: set mutation by various update() methods

---
+++ python/Sets/042.set_mutations.py
@@ -0,0 +1,21 @@
+# Problem: https://www.hackerrank.com/challenges/py-set-mutations/problem
+# Programmer: Md.Ashraful Haque
+# Date: 21.04.2024
+
+na = int(input())
+a = set(map(int, input().split()))
+n = int(input())
+
+for _ in range(n):
+    cmd, m = input().split()
+    b = set(map(int, input().split()))
+    if cmd == "update":
+        a.update(b)
+    elif cmd == "intersection_update":
+        a.intersection_update(b)
+    elif cmd == "difference_update":
+        a.difference_update(b)
+    elif cmd == "symmetric_difference_update":
+        a.symmetric_difference_update(b)
+        
+print(sum(a))
