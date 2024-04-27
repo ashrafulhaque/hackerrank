83b39852befc97665bf4cca52cbbdf473cb36712
Md.Ashraful Haque <ashrafulhaque@users.noreply.github.com>
2024-04-27 14:13:35 +0600

    Math: used pow(a,b) and pow(a,b,m)

---
+++ python/06.Math/049.power_mod_power.py
@@ -0,0 +1,10 @@
+# Problem: https://www.hackerrank.com/challenges/python-power-mod-power/problem
+# Programmer: Md.Ashraful Haque
+# Date: 27.04.2024
+
+a = int(input().strip())
+b = int(input().strip())
+m = int(input().strip())
+
+print(pow(a,b))
+print(pow(a,b,m))
