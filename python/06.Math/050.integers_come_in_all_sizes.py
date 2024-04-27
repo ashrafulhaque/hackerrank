455fea4975b16228526469c84c36d2657dbad281
Md.Ashraful Haque <ashrafulhaque@users.noreply.github.com>
2024-04-27 14:20:47 +0600

    Math: testing the pow() method for big integers

---
+++ python/06.Math/050.integers_come_in_all_sizes.py
@@ -0,0 +1,10 @@
+# Problem: https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+# Programmer: Md.Ashraful Haque
+# Date: 27.04.2024
+
+a = int(input().strip())
+b = int(input().strip())
+c = int(input().strip())
+d = int(input().strip())
+
+print(pow(a, b) + pow(c, d))
