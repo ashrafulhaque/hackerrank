dd7052a617e3d9a8127f73393e664a2c8b68aee9
Md.Ashraful Haque <ashrafulhaque@users.noreply.github.com>
2024-04-27 14:05:39 +0600

    Math: used divmod() method from Builtins

---
+++ python/06.Math/048.mod_divmod.py
@@ -0,0 +1,10 @@
+# Problem: https://www.hackerrank.com/challenges/python-mod-divmod/problem
+# Programmer: Md.Ashraful Haque
+# Date: 27.04.2024
+
+a = int(input().strip())
+b = int(input().strip())
+
+print(a//b)
+print(a%b)
+print(divmod(a,b))
